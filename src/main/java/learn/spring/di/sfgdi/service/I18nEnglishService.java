2089063705e4028a2cad570fc410f0e64242b031
Mengjun(Ivan) Wang <wmj900823@gmail.com>
2020-06-07 21:57:23 -0700

    default profile

--- src/main/java/learn/spring/di/sfgdi/service/I18nEnglishService.java
+++ src/main/java/learn/spring/di/sfgdi/service/I18nEnglishService.java
@@ -3,7 +3,7 @@ package learn.spring.di.sfgdi.service;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("en")
+@Profile({"en", "default"})
 @Service("i18nService")
 public class I18nEnglishService implements GreetingService {
     @Override
