a7c530e5527e0c7dbb8405aa91af15894e602636
Mengjun(Ivan) Wang <wmj900823@gmail.com>
2020-06-07 20:45:57 -0700

    context

---
+++ src/main/java/learn/spring/di/sfgdi/controller/MyController.java
@@ -0,0 +1,12 @@
+package learn.spring.di.sfgdi.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+    public String sayHello() {
+        System.out.println("Hello World!");
+        return "Hi Folks!";
+    }
+}
---
+++ src/test/java/learn/spring/di/sfgdi/SfgDiApplicationTests.java
@@ -0,0 +1,13 @@
+package learn.spring.di.sfgdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgDiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
